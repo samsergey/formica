59279c93383b5f8b85dc2166444f0d9bf3390b8b
Sergey (serpol) <serpol@home.ru>
2013-02-03 19:29:05 +1200

    Minor changes to examples/amb.rkt

--- examples/amb.rkt
+++ examples/amb.rkt
@@ -1,16 +1,21 @@
 #lang formica
 (define-type amb)
 
-(:: n-sqrt (Real -> (∪ (amb:) (amb: Real Real)))
-  (define (n-sqrt x)
-    (if (and (real? x) (positive? x))
-        (let ([y (sqrt x)]) 
-          (amb (- y) y))
-        (amb))))
+#;(:: real-sqrt (Real -> amb?)
+    (define (real-sqrt x)
+      (if (< x 0)
+          (amb)
+          (let ([y (sqrt x)]) 
+            (amb y (- y))))))
 
-(:: amb-plus (amb? amb? -> amb?)
+#;(:: amb-plus (amb? amb? -> amb?)
   (define/. amb-plus
     (amb x ...) (amb y ...) --> (apply amb (append x y))))
+
+(:: amb-plus (amb? amb? -> amb?)
+    (define/. amb-plus
+      (amb) x --> x
+      (amb x _ ...) _ --> (amb x)))
 
 (:: amb-bind (amb? (Any -> amb?) -> amb?)
   (define/. amb-bind
@@ -19,10 +24,19 @@
 (define-syntax amb-do
   (syntax-rules (<-)
     [(amb-do (x <- A) res) (amb-bind A (λ (x) res))]
+    [(amb-do A res) (amb-bind A (λ (_) res))]
     [(amb-do A B ... res) (amb-do A (amb-do B ... res))]))
 
-(:: assert (Any -> amb?)
-  (define (assert p)
+
+(:: guard (Any -> amb?)
+  (define (guard p)
     (if p (amb null) (amb))))
+
+(:: real-sqrt (Real -> amb?)
+  (define (real-sqrt x)
+    (amb-do (guard (>= x 0))
+            (y <- (amb (sqrt x)))
+            (amb y (- y)))))
+
 
 
