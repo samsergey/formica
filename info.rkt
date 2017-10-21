986a79e27db80dcf26be558a38cc753990716b1f
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2017-10-21 19:53:24 -0400

    Fix error on the package server.
    You can see the current error here. https://pkgs.racket-lang.org/package/formica

--- info.rkt
+++ info.rkt
@@ -2,7 +2,6 @@
 
 (define scribblings '(("doc/eng/formica.scrbl" (multi-page) (language 50))))
 
-(require string-constants)
 (define name "Formica 1.0") 
 (define compile-omit-paths '("examples" "tests" "plt"))
 (define drscheme-language-modules '(("main.rkt" "formica")))
